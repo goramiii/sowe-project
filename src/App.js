83f414c95829d8e48ebbebc179eff10b10c46588
goramiii <gorani1007@gmail.com>
2024-05-27 04:01:55 +0900

    Set up routing for multiple pages

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Order from './Order';
 import './App.css';
-import burgerImage from './assets/burger.jpg'; 
 
 function App() {
-  const [showOptions, setShowOptions] = useState(false);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="title-container">
-          <div className="title-small">어렵다 젠장 ㅠㅠ</div>
-          <div className="title-large">패스트푸드 키오스크</div>
-        </div>
-        <img src={burgerImage} alt="햄버거" className="burger-image" />
-        {!showOptions ? (
-          <div className="option-buttons">
-            <button className="order-button" onClick={() => setShowOptions(true)}>주문하기</button>
-            <button className="settings-button">상세설정</button>
-          </div>
-        ) : (
-          <div className="option-buttons">
-            <button className="eat-in-button">매장에서 먹기</button>
-            <button className="takeaway-button">포장하기</button>
-          </div>
-        )}
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/order" element={<Order />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import burgerImage from './assets/burger.jpg';
+import './App.css';
+import homeImage from './assets/home.png'; // 올바른 경로에서 이미지 임포트
 
 function Home() {
   return (
-    <div>
-      <img src={burgerImage} alt="햄버거" className="burger-image" />
-      <Link to="/order-options" className="order-button">주문하기</Link>
+    <div className="container">
+      <div className="section section1"></div>
+      <div className="section section2">
+        <img src={homeImage} alt="Home" className="background-image" /> {/* 이미지 파일 변경 */}
+        <div className="text">
+          <h1>노약자를 위한</h1>
+          <h2>음성인식 키오스크</h2>
+        </div>
+        <Link to="/order" className="order-button">주문하기</Link>
+      </div>
+      <div className="section section3"></div>
     </div>
   );
 }
